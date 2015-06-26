3541b81b89b9be176fa249887b65818d5676574f
tishambal <tishambal@gmail.com>
2015-06-26 07:57:54 +0000

    push exers

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 // CALL THE PACKAGES --------------------
 var express    = require('express');		// call express
 var app        = express(); 				// define our app using express
-var port = 8080
+var port = process.env.PORT || 8080
+var ip = process.env.IP || "127.0.0.1"
+
 
 // configure our app to handle CORS requests
 app.use(function(req, res, next) {
@@ -20,5 +22,5 @@ app.use(express.static(__dirname + '/public'));
 
 // START THE SERVER
 // ====================================
-app.listen(port);
+app.listen(port, ip);
 console.log('Magic happens on port ' + port);
